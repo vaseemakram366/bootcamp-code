23a04963c72d6eca3bd2f9dcc8960ba1c08760a2
Vaseem Akram <vaseemakrama366@gmail.com>
2026-06-19 19:38:12 +0530

    Added React Day5 project

---
+++ week2/day5/src/App.jsx
@@ -0,0 +1,19 @@
+import Card from "./components/Card";
+
+function App() {
+  return (
+    <div> <h1>Hello World 🚀</h1>
+    
+
+    <img src="https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcT7OwwUrrG1T1ewtLgFA60xsiawHDB3gl3HG0mUx0BziOq6U9gq_cuU8DQftKrEqIkJVz2j5RdNXd3pdefSk5bCHy2Ztyue-SP02mWYtD-FxrObYgvw1nXUtC48sdcHewLOIOc_8X2oMAs&s=19" alt="" />
+
+    <h1>Ronaldo</h1>
+    <Card/>
+    <button>Goal!</button>
+
+    </div>
+    
+  )
+}
+
+export default App;
---
+++ week2/day5/src/components/Card.jsx
@@ -0,0 +1,11 @@
+function Card() {
+    return (
+        <div>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTh4gvwJOS20xgVhyxaBjxCtNvJ3UsxEL8dag&s" alt="" />
+            <h1>Name:john</h1>
+            <button>Know more</button>
+        </div>
+    );
+}
+
+export default Card;
